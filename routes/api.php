704baf763afcdd04a1eb5713e7fe33a7d6e6e2fb
Harold Cortes <harcor59p@gmail.com>
2023-12-05 20:18:38 -0500

    ultimos cambios

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProductosController;
 
 /*
