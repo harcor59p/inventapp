e0f88030d4b2d0418db81dfae2eef37f2a915745
Harold Cortes <harcor59p@gmail.com>
2023-12-11 19:26:34 -0500

    creacion cors

--- routes/api.php
+++ routes/api.php
@@ -23,37 +23,41 @@ use App\Http\Controllers\API\TiposTransaccionesController;
     return $request->user();
 }); */
 
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::group(['middleware' => ['cors']], function () {
 
-Route::prefix('productos')->group(function () {
-    Route::get('/', [ProductosController::class, 'get']);
-    Route::get('/{id}', [ProductosController::class, 'getById']);
-    Route::put('/{id}', [ProductosController::class, 'update']);
-    Route::delete('/{id}', [ProductosController::class, 'delete']);
-    Route::post('/', [ProductosController::class, 'create']);
-});
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::prefix('bodegas')->group(function () {
-    Route::get('/', [BodegasController::class, 'get']);
-    Route::get('/{id}', [BodegasController::class, 'getById']);
-    Route::put('/{id}', [BodegasController::class, 'update']);
-    Route::delete('/{id}', [BodegasController::class, 'delete']);
-    Route::post('/', [BodegasController::class, 'create']);
-});
+    Route::prefix('productos')->group(function () {
+        Route::get('/', [ProductosController::class, 'get']);
+        Route::get('/{id}', [ProductosController::class, 'getById']);
+        Route::put('/{id}', [ProductosController::class, 'update']);
+        Route::delete('/{id}', [ProductosController::class, 'delete']);
+        Route::post('/', [ProductosController::class, 'create']);
+    });
 
-Route::prefix('GruposProductos')->group(function () {
-    Route::get('/', [GruposProductosController::class, 'get']);
-    Route::get('/{id}', [GruposProductosController::class, 'getById']);
-    Route::put('/{id}', [GruposProductosController::class, 'update']);
-    Route::delete('/{id}', [GruposProductosController::class, 'delete']);
-    Route::post('/', [GruposProductosController::class, 'create']);
-});
+    Route::prefix('bodegas')->group(function () {
+        Route::get('/', [BodegasController::class, 'get']);
+        Route::get('/{id}', [BodegasController::class, 'getById']);
+        Route::put('/{id}', [BodegasController::class, 'update']);
+        Route::delete('/{id}', [BodegasController::class, 'delete']);
+        Route::post('/', [BodegasController::class, 'create']);
+    });
 
-Route::prefix('TiposTransacciones')->group(function () {
-    Route::get('/', [TiposTransaccionesController::class, 'get']);
-    Route::get('/{id}', [TiposTransaccionesController::class, 'getById']);
-    Route::put('/{id}', [TiposTransaccionesController::class, 'update']);
-    Route::delete('/{id}', [TiposTransaccionesController::class, 'delete']);
-    Route::post('/', [TiposTransaccionesController::class, 'create']);
+    Route::prefix('GruposProductos')->group(function () {
+        Route::get('/', [GruposProductosController::class, 'get']);
+        Route::get('/{id}', [GruposProductosController::class, 'getById']);
+        Route::put('/{id}', [GruposProductosController::class, 'update']);
+        Route::delete('/{id}', [GruposProductosController::class, 'delete']);
+        Route::post('/', [GruposProductosController::class, 'create']);
+    });
+
+    Route::prefix('TiposTransacciones')->group(function () {
+        Route::get('/', [TiposTransaccionesController::class, 'get']);
+        Route::get('/{id}', [TiposTransaccionesController::class, 'getById']);
+        Route::put('/{id}', [TiposTransaccionesController::class, 'update']);
+        Route::delete('/{id}', [TiposTransaccionesController::class, 'delete']);
+        Route::post('/', [TiposTransaccionesController::class, 'create']);
+    });
+
 });
